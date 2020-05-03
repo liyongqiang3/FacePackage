4e09bcb0200aa204240204ce1af0de6b01e98ff1
liyongqiang <liyongqiang02@meituan.com>
2020-05-03 19:29:16 +0800

    修改定义

---
+++ FacePackage/Classes/LZFacePackageBoard.h
@@ -0,0 +1,16 @@
+//
+//  LZFacePackageBoard.h
+//  Masonry
+//
+//  Created by yongqiang li on 2020/5/3.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LZFacePackageBoard : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FacePackage/Classes/LZFacePackageBoard.m
@@ -0,0 +1,20 @@
+//
+//  LZFacePackageBoard.m
+//  Masonry
+//
+//  Created by yongqiang li on 2020/5/3.
+//
+
+#import "LZFacePackageBoard.h"
+
+@implementation LZFacePackageBoard
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
